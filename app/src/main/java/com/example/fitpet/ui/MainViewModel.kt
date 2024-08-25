4f3499129a66c2f6d88b343cee278ed218171dde
dogmania <hj296914@gmail.com>
2024-08-25 20:03:44 +0900

    :sparkles: Feat: MainViewModel 구현

---
+++ app/src/main/java/com/example/fitpet/ui/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.fitpet.ui
+
+import com.example.fitpet.PageState
+import com.example.fitpet.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): BaseViewModel<MainPageState>(
+    initialState = MainPageState()
+) {
+
+}
