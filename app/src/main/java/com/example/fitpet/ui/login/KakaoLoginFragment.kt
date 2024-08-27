00c3ba1b7cba95453819cf8bd35242b61b5b279d
dogmania <hj296914@gmail.com>
2024-08-27 15:54:54 +0900

    :sparkles: Feat: 카카오 로그인 구현

--- app/src/main/java/com/example/fitpet/ui/login/KakaoLoginFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/login/KakaoLoginFragment.kt
@@ -1,31 +1,71 @@
 package com.example.fitpet.ui.login
 
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.fitpet.R
+import com.example.fitpet.PageState
+import com.example.fitpet.base.BaseFragment
+import com.example.fitpet.databinding.FragmentKakaoLoginBinding
+import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class KakaoLoginFragment : Fragment() {
+@AndroidEntryPoint
+class KakaoLoginFragment : BaseFragment<FragmentKakaoLoginBinding, PageState.Default, KakaoLoginViewModel>(
+    FragmentKakaoLoginBinding::inflate
+) {
+    override val viewModel: KakaoLoginViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = KakaoLoginFragment()
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+        }
     }
 
-    private val viewModel: KakaoLoginViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    override fun initState() {
+        launchWhenStarted(viewLifecycleOwner) {
+            launch {
+                viewModel.eventFlow.collect { event ->
+                    handleEvent(event as KakaoLoginEvent)
+                }
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_kakao_login, container, false)
+    private fun handleEvent(event: KakaoLoginEvent) {
+        when (event) {
+            KakaoLoginEvent.OnClickKakaoLogin -> signInKakao()
+        }
+    }
+
+    private fun signInKakao() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            signInKakaoApp()
+        } else {
+            signInKakaoEmail()
+        }
+    }
+
+    private fun signInKakaoApp() {
+        UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+            if (error != null) {
+                Timber.tag("KaKao Login Error").e(error.stackTraceToString())
+                return@loginWithKakaoTalk
+            }
+
+            Toast.makeText(requireContext(), "로그인 성공하였습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signInKakaoEmail() {
+        UserApiClient.instance.loginWithKakaoAccount(requireContext()) { token, error ->
+            if (error != null) {
+                Timber.tag("KaKao Login Error").e(error.stackTraceToString())
+                return@loginWithKakaoAccount
+            }
+
+            Toast.makeText(requireContext(), "로그인 성공하였습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/fitpet/ui/login/KakaoLoginViewModel.kt
+++ app/src/main/java/com/example/fitpet/ui/login/KakaoLoginViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.fitpet.ui.login
 
 import androidx.lifecycle.ViewModel
+import com.example.fitpet.PageState
+import com.example.fitpet.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class KakaoLoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class KakaoLoginViewModel @Inject constructor() : BaseViewModel<PageState.Default>() {
+    override val uiState: PageState.Default
+        get() = TODO("Not yet implemented")
+
+    fun onClickBtnKakaoLogin() {
+        emitEventFlow(KakaoLoginEvent.OnClickKakaoLogin)
+    }
 }
