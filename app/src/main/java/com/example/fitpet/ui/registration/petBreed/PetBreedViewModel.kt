781fd2b58482e9348447fe1155c41794c57ceef2
dogmania <hj296914@gmail.com>
2024-08-28 21:13:05 +0900

    :sparkles: Feat: PetBreedViewModel 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petBreed/PetBreedViewModel.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petBreed/PetBreedViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.fitpet.ui.registration.petBreed
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitpet.base.BaseViewModel
+import com.example.fitpet.model.domain.PetType
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PetBreedViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class PetBreedViewModel @Inject constructor() : BaseViewModel<PetBreedPageState>() {
+    private val breedStateFlow: MutableStateFlow<PetType> = MutableStateFlow(PetType.DOG)
+
+    override val uiState: PetBreedPageState = PetBreedPageState(
+        breed = breedStateFlow.asStateFlow()
+    )
+
+    fun onClickBtnBreed(newType: PetType) {
+        if (breedStateFlow.value == newType) return
+
+        viewModelScope.launch {
+            breedStateFlow.update { newType }
+        }
+    }
 }
