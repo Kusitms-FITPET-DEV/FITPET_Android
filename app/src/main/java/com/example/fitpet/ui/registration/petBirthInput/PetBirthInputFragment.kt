08273107464de118ecaf0baf4f1b834286e1f843
dogmania <hj296914@gmail.com>
2024-08-28 23:05:02 +0900

    :sparkles: Feat: PetBirthInputFragment 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputFragment.kt
@@ -1,31 +1,36 @@
 package com.example.fitpet.ui.registration.petBirthInput
 
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.fitpet.R
+import com.example.fitpet.base.BaseFragment
+import com.example.fitpet.databinding.FragmentPetBirthInputBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class PetBirthInputFragment : Fragment() {
+@AndroidEntryPoint
+class PetBirthInputFragment : BaseFragment<FragmentPetBirthInputBinding, PetBirthInputPageState, PetBirthInputViewModel>(
+    FragmentPetBirthInputBinding::inflate
+) {
+    override val viewModel: PetBirthInputViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = PetBirthInputFragment()
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+        }
     }
 
-    private val viewModel: PetBirthInputViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    override fun initState() {
+        launchWhenStarted(viewLifecycleOwner) {
+            launch {
+                viewModel.eventFlow.collect { event ->
+                    handleEvent(event as PetBirthInputEvent)
+                }
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_pet_birth_input, container, false)
+    private fun handleEvent(event: PetBirthInputEvent) {
+        when(event) {
+            PetBirthInputEvent.GoToContactInfoInput -> TODO()
+        }
     }
 }
