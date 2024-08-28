e0e6d8531c6ea660de3453a1611d7076d661b078
dogmania <hj296914@gmail.com>
2024-08-28 21:12:49 +0900

    :sparkles: Feat: PetBreedPageState 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petBreed/PetBreedPageState.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petBreed/PetBreedPageState.kt
@@ -1,4 +1,9 @@
 package com.example.fitpet.ui.registration.petBreed
 
-class PetBreedPageState {
-}
+import com.example.fitpet.PageState
+import com.example.fitpet.model.domain.PetType
+import kotlinx.coroutines.flow.StateFlow
+
+data class PetBreedPageState(
+    val breed: StateFlow<PetType>
+): PageState
