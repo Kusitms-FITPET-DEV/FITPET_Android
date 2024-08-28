e292ee6da9b9a74eaad15f8986cc0404676d1216
dogmania <hj296914@gmail.com>
2024-08-28 21:11:06 +0900

    :sparkles: Feat: PetNameInputFragment -> PetBreedFragment 내비게이션 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputFragment.kt
@@ -1,8 +1,10 @@
 package com.example.fitpet.ui.registration.petName
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.fitpet.base.BaseFragment
 import com.example.fitpet.databinding.FragmentPetNameInputBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class PetNameInputFragment : BaseFragment<FragmentPetNameInputBinding, PetNameInputPageState, PetNameInputViewModel>(
@@ -20,10 +22,21 @@ class PetNameInputFragment : BaseFragment<FragmentPetNameInputBinding, PetNameIn
     override fun initState() {
         launchWhenStarted(viewLifecycleOwner) {
             launch {
-
+                viewModel.eventFlow.collect { event ->
+                    handleEvent(event as PetNameInputEvent)
+                }
             }
         }
     }
 
+    private fun handleEvent(event: PetNameInputEvent) {
+        when (event) {
+            PetNameInputEvent.GoToPetBreedInput -> goToPetBreedInput()
+        }
+    }
 
+    private fun goToPetBreedInput() {
+        val action = PetNameInputFragmentDirections.actionPetNameInputToPetBreed()
+        findNavController().navigate(action)
+    }
 }
