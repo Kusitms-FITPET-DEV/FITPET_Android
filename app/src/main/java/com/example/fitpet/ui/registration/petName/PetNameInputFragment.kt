2fa8b5544771cd0b2243bfbacedb398833d1a695
dogmania <hj296914@gmail.com>
2024-08-28 19:57:51 +0900

    :sparkles: Feat: PetNameInputFragment 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputFragment.kt
@@ -1,31 +1,29 @@
 package com.example.fitpet.ui.registration.petName
 
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.fitpet.R
+import com.example.fitpet.base.BaseFragment
+import com.example.fitpet.databinding.FragmentPetNameInputBinding
+import kotlinx.coroutines.launch
 
-class PetNameInputFragment : Fragment() {
+class PetNameInputFragment : BaseFragment<FragmentPetNameInputBinding, PetNameInputPageState, PetNameInputViewModel>(
+    FragmentPetNameInputBinding::inflate
+) {
 
-    companion object {
-        fun newInstance() = PetNameInputFragment()
+    override val viewModel: PetNameInputViewModel by viewModels()
+
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+        }
     }
 
-    private val viewModel: PetNameInputViewModel by viewModels()
+    override fun initState() {
+        launchWhenStarted(viewLifecycleOwner) {
+            launch {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_pet_name_input, container, false)
-    }
+
 }
