8580d441626a1ec503f93b2549bc4a8307652a04
dogmania <hj296914@gmail.com>
2024-08-28 23:04:48 +0900

    :sparkles: Feat: PetBirthInputEvent 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputEvent.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputEvent.kt
@@ -1,4 +1,7 @@
 package com.example.fitpet.ui.registration.petBirthInput
 
-class PetBirthInputEvent {
+import com.example.fitpet.Event
+
+sealed class PetBirthInputEvent: Event {
+    data object GoToContactInfoInput: PetBirthInputEvent()
 }
