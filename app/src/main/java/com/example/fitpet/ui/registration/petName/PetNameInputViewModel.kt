bf95c468815a9ac1824ac35432918b0ab25dd976
dogmania <hj296914@gmail.com>
2024-08-28 19:58:25 +0900

    :sparkles: Feat: PetNameInputViewModel 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputViewModel.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petName/PetNameInputViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.fitpet.ui.registration.petName
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitpet.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PetNameInputViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class PetNameInputViewModel @Inject constructor(
+
+) : BaseViewModel<PetNameInputPageState>() {
+    private val petNameStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+
+    override val uiState: PetNameInputPageState = PetNameInputPageState(
+        petName = petNameStateFlow.asStateFlow()
+    )
+
+    fun onTextChanged(newText: CharSequence) {
+        viewModelScope.launch {
+            petNameStateFlow.update { newText.toString() }
+        }
+    }
 }
