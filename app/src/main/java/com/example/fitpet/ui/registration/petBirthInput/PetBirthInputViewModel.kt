d0c366f4731c21b9882758ad3912814dc1935a4b
dogmania <hj296914@gmail.com>
2024-08-28 23:05:31 +0900

    :sparkles: Feat: PetBirthInputViewModel 구현

--- app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputViewModel.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petBirthInput/PetBirthInputViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.fitpet.ui.registration.petBirthInput
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fitpet.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PetBirthInputViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class PetBirthInputViewModel @Inject constructor(
+
+) : BaseViewModel<PetBirthInputPageState>() {
+    private val birthStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+
+    override val uiState: PetBirthInputPageState = PetBirthInputPageState(
+        birth = birthStateFlow.asStateFlow()
+    )
+
+    fun onTextChanged(input: CharSequence) {
+        viewModelScope.launch {
+            birthStateFlow.update { input.toString() }
+        }
+    }
+
+    fun onClickBtnNext() {
+        emitEventFlow(PetBirthInputEvent.GoToContactInfoInput)
+    }
 }
