505b548fce502de4b9bf11ec251d124769c4cef6
dogmania <hj296914@gmail.com>
2024-08-31 03:18:13 +0900

    :sparkles: Feat: 권한 설정 바텀시트 구현

--- app/src/main/java/com/example/fitpet/ui/onboarding/bottomsheet/PermissionSettingsBottomSheet.kt
+++ app/src/main/java/com/example/fitpet/ui/onboarding/bottomsheet/PermissionSettingsBottomSheet.kt
@@ -1,64 +1,36 @@
 package com.example.fitpet.ui.onboarding.bottomsheet
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fitpet.R
+import com.example.fitpet.databinding.FragmentPermissionSettingsBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PermissionSettingsBottomSheet.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PermissionSettingsBottomSheet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class PermissionSettingsBottomSheet(
+    private val onClickBtnAgreement: () -> Unit
+) : BottomSheetDialogFragment() {
+    private val binding: FragmentPermissionSettingsBottomSheetBinding by lazy {
+        FragmentPermissionSettingsBottomSheetBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_permission_settings_bottom_sheet,
-            container,
-            false
-        )
+    ): View {
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PermissionSettingsBottomSheet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PermissionSettingsBottomSheet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnAgreement.setOnClickListener {
+                onClickBtnAgreement()
+                dismiss()
             }
+        }
     }
 }
