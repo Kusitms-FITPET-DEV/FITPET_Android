b7b59f12d1eb10b6238fa99bd81cbc25bce03395
emjayMJkim <minjeong030420@gmail.com>
2024-08-31 20:38:01 +0900

    :sparkles: Feat: 보험금 청구 캘린더 페이지 화면 구현

---
+++ app/src/main/java/com/example/fitpet/ui/insurance/charge/cause/CalendarBottomSheet.kt
@@ -0,0 +1,37 @@
+package com.example.fitpet.ui.insurance.charge.cause
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitpet.databinding.BottomSheetCalendarBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CalendarBottomSheet: BottomSheetDialogFragment() {
+
+    private var _binding: BottomSheetCalendarBinding? = null
+    val binding: BottomSheetCalendarBinding
+        get() = requireNotNull(_binding as BottomSheetCalendarBinding)
+
+    override fun onStart() {
+        super.onStart()
+
+        val behavior = BottomSheetBehavior.from(requireView().parent as View)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = BottomSheetCalendarBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
