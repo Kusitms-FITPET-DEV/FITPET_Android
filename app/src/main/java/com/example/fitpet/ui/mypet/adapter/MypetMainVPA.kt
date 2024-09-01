339b6f226218918203b94f146357e9e63ca59e38
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-09-01 14:09:07 +0900

    :bento: Chore: companion object 사용

--- app/src/main/java/com/example/fitpet/ui/mypet/adapter/MypetMainVPA.kt
+++ app/src/main/java/com/example/fitpet/ui/mypet/adapter/MypetMainVPA.kt
@@ -7,12 +7,18 @@ import com.example.fitpet.ui.mypet.insurance.InsuranceFragment
 import com.example.fitpet.ui.mypet.life.LifeFragment
 
 class MypetMainVPA(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 2
+    companion object {
+        private const val INSURANCE_FRAGMENT_POSITION = 0
+        private const val LIFE_FRAGMENT_POSITION = 1
+        private const val TOTAL_COUNT = 2
+    }
+
+    override fun getItemCount(): Int = TOTAL_COUNT
 
     override fun createFragment(position: Int): Fragment {
-        return when(position){
-            0 -> InsuranceFragment()
-            1 -> LifeFragment()
+        return when (position) {
+            INSURANCE_FRAGMENT_POSITION -> InsuranceFragment()
+            LIFE_FRAGMENT_POSITION -> LifeFragment()
             else -> throw IllegalStateException("Invalid position")
         }
     }
