7f215aff0b67986b7bfa5e20e7114ac6c6758d23
emjayMJkim <minjeong030420@gmail.com>
2024-09-03 21:36:47 +0900

    :sparkles: Feat: 사진 찍기 클릭 리스너 추가

---
+++ app/src/main/java/com/example/fitpet/ui/insurance/charge/document/photo/AddPhotoEvent.kt
@@ -0,0 +1,7 @@
+package com.example.fitpet.ui.insurance.charge.document.photo
+
+import com.example.fitpet.Event
+
+sealed class AddPhotoEvent: Event {
+    data object ClickCamera: AddPhotoEvent()
+}
