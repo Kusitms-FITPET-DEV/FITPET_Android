a0defe61f95b4c653679f4a47a23e90ab3297055
dogmania <hj296914@gmail.com>
2024-09-04 11:09:59 +0900

    :sparkles: Feat: refreshToken 저장 로직 추가

--- app/src/main/java/com/example/fitpet/data/FitPetDataStore.kt
+++ app/src/main/java/com/example/fitpet/data/FitPetDataStore.kt
@@ -1,5 +1,6 @@
 package com.example.fitpet.data
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import androidx.datastore.preferences.core.edit
@@ -7,14 +8,17 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 
 private val Context.dataStore by preferencesDataStore(name = "fit_pet_prefs")
 
+@SuppressLint("TimberArgCount")
 class FitPetDataStore(context: Context) {
     private val dataStore = context.dataStore
 
     companion object {
         val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token")
+        val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
     }
 
     val accessToken: Flow<String?> = dataStore.data
@@ -22,10 +26,22 @@ class FitPetDataStore(context: Context) {
             preferences[ACCESS_TOKEN_KEY]
         }
 
+    val refreshToken: Flow<String?> = dataStore.data
+        .map { preferences ->
+            preferences[REFRESH_TOKEN_KEY]
+        }
+
     suspend fun saveAccessToken(token: String) {
         dataStore.edit { preferences ->
             preferences[ACCESS_TOKEN_KEY] = token
-            Log.i("Access Token", token)
+            Timber.i("Access Token", token)
+        }
+    }
+
+    suspend fun saveRefreshToken(token: String) {
+        dataStore.edit { preferences ->
+            preferences[REFRESH_TOKEN_KEY] = token
+            Timber.i("Refresh Token", token)
         }
     }
 }
