0600f59754702df11d000b9e8a6c2e3da9540aaa
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-09-05 19:17:46 +0900

    :sparkles: Feat: 두 번째 메인 화면 기능 구현
    - data binding 추가
    - RVA 구현
    - model 추가

---
+++ app/src/main/java/com/example/fitpet/ui/mypet/insurance/noregister/InsuranceNoRegisterEvent.kt
@@ -0,0 +1,11 @@
+package com.example.fitpet.ui.mypet.insurance.noregister
+
+import com.example.fitpet.Event
+
+sealed class InsuranceNoRegisterEvent: Event {
+    data object GoToConsult: InsuranceNoRegisterEvent()
+    data object OpenMyPetDialog: InsuranceNoRegisterEvent()
+    data object ChangeRange: InsuranceNoRegisterEvent()
+    data object ShowNothing: InsuranceNoRegisterEvent()
+
+}
