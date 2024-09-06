eaa2b77ce324234dec5e4bf9daf28ea3bf2f3429
dogmania <hj296914@gmail.com>
2024-09-06 13:31:07 +0900

    :sparkles: Feat: 품종 자동완성 디자인 변경

--- app/src/main/java/com/example/fitpet/ui/registration/petDetailBreed/PetDetailBreedInputFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/registration/petDetailBreed/PetDetailBreedInputFragment.kt
@@ -1,29 +1,15 @@
 package com.example.fitpet.ui.registration.petDetailBreed
 
-import android.content.Context
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
-import androidx.appcompat.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitpet.R
 import com.example.fitpet.base.BaseFragment
 import com.example.fitpet.databinding.FragmentPetDetailBreedInputBinding
 import com.example.fitpet.ui.onboarding.dialog.SkipDialog
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -46,7 +32,12 @@ class PetDetailBreedInputFragment : BaseFragment<FragmentPetDetailBreedInputBind
             val args: PetDetailBreedInputFragmentArgs by navArgs()
             petName = args.petName
             petBreed = args.petBreed
-            breedAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, viewModel.uiState.searchedBreedList.value)
+
+            breedAdapter = PetDetailBreedArrayAdapter(
+                requireContext(),
+                viewModel.uiState.searchedBreedList.value,
+                viewModel.uiState.detailBreed.value
+            )
 
             detailBreedAutoCompleteTextView.apply {
                 setAdapter(breedAdapter)
@@ -58,6 +49,7 @@ class PetDetailBreedInputFragment : BaseFragment<FragmentPetDetailBreedInputBind
                     }
                     dismissDropDown()
                 }
+                setDropDownBackgroundResource(R.drawable.bg_rectangle_filled_bg_primary_radius_8)
                 dropDownAnchor = root.id
             }
         }
@@ -68,7 +60,11 @@ class PetDetailBreedInputFragment : BaseFragment<FragmentPetDetailBreedInputBind
             launch {
                 viewModel.uiState.searchedBreedList.collect { breedList ->
                     Timber.e("업데이트 됐다 $breedList")
-                    breedAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, breedList)
+                    breedAdapter = PetDetailBreedArrayAdapter(
+                        requireContext(),
+                        viewModel.uiState.searchedBreedList.value,
+                        viewModel.uiState.detailBreed.value
+                    )
                     binding.detailBreedAutoCompleteTextView.showDropDown()
                     binding.detailBreedAutoCompleteTextView.setAdapter(breedAdapter)
                 }
