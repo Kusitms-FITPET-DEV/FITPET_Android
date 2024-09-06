43c8d43d9fc6776a6642d4af9faa2f3755db6090
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-09-06 21:51:20 +0900

    :sparkles: Feat: PageState 설정

--- app/src/main/java/com/example/fitpet/ui/mypet/insurance/main/InsuranceMainPageState.kt
+++ app/src/main/java/com/example/fitpet/ui/mypet/insurance/main/InsuranceMainPageState.kt
@@ -1,7 +1,11 @@
 package com.example.fitpet.ui.mypet.insurance.main
 
 import com.example.fitpet.PageState
+import com.example.fitpet.model.domain.insurance.main.MyPet
+import kotlinx.coroutines.flow.StateFlow
 
 data class InsuranceMainPageState(
-    val temp: String = ""
+    var petInfo: StateFlow<MyPet>,
+    var insurancePrice: StateFlow<Int>,
+    var nowMonth: StateFlow<Int>,
 ): PageState
