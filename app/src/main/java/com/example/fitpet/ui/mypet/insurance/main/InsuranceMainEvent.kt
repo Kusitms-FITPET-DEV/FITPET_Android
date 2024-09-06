d569f11a23818cfa5a911984f8879378411705f6
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-09-06 21:51:56 +0900

    :sparkles: Feat: Event 설정

--- app/src/main/java/com/example/fitpet/ui/mypet/insurance/main/InsuranceMainEvent.kt
+++ app/src/main/java/com/example/fitpet/ui/mypet/insurance/main/InsuranceMainEvent.kt
@@ -3,5 +3,8 @@ package com.example.fitpet.ui.mypet.insurance.main
 import com.example.fitpet.Event
 
 sealed class InsuranceMainEvent: Event {
-
+    data object GoToConsult: InsuranceMainEvent()
+    data object GoToCharge: InsuranceMainEvent()
+    data object GoToCheck: InsuranceMainEvent()
+    data object OpenMyPetDialog: InsuranceMainEvent()
 }
