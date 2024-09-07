0c8e505f1b47a511bc0876a91b360e87c6a58ffa
emjayMJkim <minjeong030420@gmail.com>
2024-09-07 22:43:37 +0900

    :sparkles: Feat: 보상 내역 조회 의존성 주입

--- app/src/main/java/com/example/fitpet/di/RepositoryModule.kt
+++ app/src/main/java/com/example/fitpet/di/RepositoryModule.kt
@@ -2,9 +2,11 @@ package com.example.fitpet.di
 
 import com.example.fitpet.data.repository.AuthRepository
 import com.example.fitpet.data.repository.ChargeRepository
+import com.example.fitpet.data.repository.InsuranceDetailRepository
 import com.example.fitpet.data.repository.PetsRepository
 import com.example.fitpet.data.repositoryImpl.AuthRepositoryImpl
 import com.example.fitpet.data.repositoryImpl.ChargeRepositoryImpl
+import com.example.fitpet.data.repositoryImpl.InsuranceDetailRepositoryImpl
 import com.example.fitpet.data.repositoryImpl.PetsRepositoryImpl
 import dagger.Binds
 import dagger.Module
@@ -26,4 +28,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun providesChargeUploadRepository(repositoryImpl: ChargeRepositoryImpl): ChargeRepository
+
+    @Singleton
+    @Binds
+    abstract fun proveidesInsuranceDetailRepository(repositoryImpl: InsuranceDetailRepositoryImpl): InsuranceDetailRepository
 }
