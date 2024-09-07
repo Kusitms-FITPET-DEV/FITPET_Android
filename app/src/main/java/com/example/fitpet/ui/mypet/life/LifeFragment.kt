7a4392d77cef1708658321de714964a4e14f99d1
dogmania <hj296914@gmail.com>
2024-09-07 20:19:03 +0900

    :sparkles: Feat: 반려생활 페이지 구현

--- app/src/main/java/com/example/fitpet/ui/mypet/life/LifeFragment.kt
+++ app/src/main/java/com/example/fitpet/ui/mypet/life/LifeFragment.kt
@@ -3,6 +3,7 @@ package com.example.fitpet.ui.mypet.life
 import androidx.fragment.app.viewModels
 import com.example.fitpet.base.BaseFragment
 import com.example.fitpet.databinding.FragmentLifeBinding
+import com.example.fitpet.ui.mypet.life.adapter.AdvertisementAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +11,14 @@ class LifeFragment : BaseFragment<FragmentLifeBinding, LifePageState, LifeViewMo
     FragmentLifeBinding::inflate
 ) {
     override val viewModel: LifeViewModel by viewModels()
+
+    private val adapter by lazy {
+        AdvertisementAdapter(object: AdvertisementAdapter.OnItemChange {
+            override fun onItemChange(id: Long) {
+                viewModel.updatePageCount(id.toInt())
+            }
+        })
+    }
 
     override fun initView() {
 
