3064687e09523f2d177110b8505a80b96f83f79f
dogmania <hj296914@gmail.com>
2024-09-08 18:15:50 +0900

    :recycle: Refactor: 태그 추가

--- app/src/main/java/com/example/fitpet/ui/mypet/life/faq/adapter/FaqViewHolder.kt
+++ app/src/main/java/com/example/fitpet/ui/mypet/life/faq/adapter/FaqViewHolder.kt
@@ -1,16 +1,21 @@
 package com.example.fitpet.ui.mypet.life.faq.adapter
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fitpet.R
 import com.example.fitpet.databinding.ItemFaqBinding
+import com.example.fitpet.model.domain.FaqType
 import com.example.fitpet.model.response.FaqItemVo
 
 class FaqViewHolder(
     private val binding: ItemFaqBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    @SuppressLint("SetTextI18n")
     fun bind(item: FaqItemVo) {
+        val context = binding.root.context
+
         binding.apply {
             var isAnswerVisible = false
 
@@ -33,6 +38,15 @@ class FaqViewHolder(
                         }.start()
                     binding.btnToggle.setImageResource(R.drawable.ic_arrow_dorp_down)
                 }
+            }
+
+            when(item.type) {
+                FaqType.REWARD -> tag.text = "# ${context.getString(R.string.reward)}"
+                FaqType.INSURANCE_FEE -> tag.text = "# ${context.getString(R.string.insurance_fee)}"
+                FaqType.COVERAGE_RANGE -> tag.text = "# ${context.getString(R.string.coverage_range)}"
+                FaqType.COVERAGE_PERIOD -> tag.text = "# ${context.getString(R.string.coverage_period)}"
+                FaqType.SUBSCRIPTION_TERM -> tag.text = "# ${context.getString(R.string.subscription_term)}"
+                FaqType.OTHER_INFO -> tag.text = "# ${context.getString(R.string.other_info)}"
             }
         }
     }
